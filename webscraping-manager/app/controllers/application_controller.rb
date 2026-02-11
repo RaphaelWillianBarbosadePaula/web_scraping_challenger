504e85fc4cedf094342c0afdb4cb5de2ac42be85
Raphael Willian <raphaelwillian.bdp44@gmail.com>
2026-02-10 23:01:20 -0300

    feat: add authentication user for system's features

--- webscraping-manager/app/controllers/application_controller.rb
+++ webscraping-manager/app/controllers/application_controller.rb
@@ -1,4 +1,38 @@
+require 'ostruct'
+
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_user, :user_signed_in?
+
+  private
+
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to login_path, alert: "Você precisa fazer login para acessar essa página."
+    end
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+
+    token = session[:jwt_token]
+    return nil if token.blank?
+
+    begin
+      decoded_token = JWT.decode(token, nil, false).first
+
+      user_id = decoded_token['user_id'] || decoded_token['sub'] || decoded_token['id']
+
+      @current_user = OpenStruct.new(id: user_id, email: decoded_token['email'], nickname: decoded_token['nickname'])
+    rescue JWT::DecodeError
+      session[:jwt_token] = nil
+      @current_user = nil
+      nil
+    end
+  end
 end
