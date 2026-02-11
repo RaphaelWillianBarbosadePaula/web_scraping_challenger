716b75ccad8b5739fab00e49decf2481443e3ae4
Raphael Willian <raphaelwillian.bdp44@gmail.com>
2026-02-11 20:36:30 -0300

    feat: add index to notifications_controller of notification-service

--- notification-service/app/controllers/notifications_controller.rb
+++ notification-service/app/controllers/notifications_controller.rb
@@ -1,4 +1,15 @@
 class NotificationsController < ApplicationController
+  def index
+    scope = Notification.all
+
+    scope = scope.where(task_id: params[:task_id]) if params[:task_id].present?
+    scope = scope.where(user_id: params[:user_id]) if params[:user_id].present?
+
+    notifications = scope.order(created_at: :desc).limit(100)
+
+    render json: notifications
+  end
+
   def create
     notification = Notification.new(notification_params)
     if notification.save
--- notification-service/config/routes.rb
+++ notification-service/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post '/notifications', to: 'notifications#create'
+  get '/notifications', to: 'notifications#index'
   get "up" => "rails/health#show", as: :rails_health_check
 end
